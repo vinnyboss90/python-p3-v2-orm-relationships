74ce49bd183aa1f04c1ee5546afeca7d48286156
vinnyegon90 <vinnyegon90@gmail.com>
2024-03-18 14:33:26 +0300

    Completed

--- lib/department.py
+++ lib/department.py
@@ -2,7 +2,6 @@
 
 from __init__ import CURSOR, CONN
 
-
 class Department:
 
     # Dictionary of objects saved to the database.
@@ -139,3 +138,14 @@ class Department:
 
         row = CURSOR.execute(sql, (name,)).fetchone()
         return cls.instance_from_db(row) if row else None
+    
+    def employees(self):
+        """Return list of employees associated with current department"""
+        from employee import Employee
+        sql = """
+            SELECT * FROM employees
+            WHERE department_id = ?
+        """
+        CURSOR.execute(sql, (self.id,),)
+        rows = CURSOR.fetchall()
+        return [Employee.instance_from_db(row) for row in rows]
